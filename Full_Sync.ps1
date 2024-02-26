68039f276c5aa92cd7c572a74c70a411b7ca1977
Kevin-blum <160986057+Kevin-blum@users.noreply.github.com>
2024-02-26 03:20:04 +0100

    Add files via upload

---
+++ Full_Sync.ps1
@@ -0,0 +1,12 @@
+# Version 1.0
+# Author: Kevin Blum
+
+# Enter the Server Name
+$ServerName = Read-Host "Enter the Server Name"
+
+# Delta Sync
+Enter-PSSession -ComputerName $ServerName
+Start-ADSyncSyncCycle -PolicyType Initial
+
+
+
